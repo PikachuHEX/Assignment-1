173356872eae8c078305085aba5b2237bdc430b6
PikachuHEX <me.saurav2015@gmail.com>
2022-10-26 22:52:49 -0400

    just2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var contactRouter = require('./server/routes/contact');
 
 
 //modules for authentication 
-let session = require('express-session');
+let session = require('cookie-session');
 let passport = require('passport');
 let passportLocal = require('passport-local');
 let localStrategy = passportLocal.Strategy;
